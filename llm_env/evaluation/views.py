86562ba82912c249adf33a30014376008c5d3087
aceail <38589098+aceail@users.noreply.github.com>
2025-07-18 16:08:13 +0900

    Add evaluation page template

--- llm_env/evaluation/views.py
+++ llm_env/evaluation/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def evaluation_view(request):
+    """Render a static evaluation page for testing."""
+    return render(request, 'evaluation/evaluation.html')
