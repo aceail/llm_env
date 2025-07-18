92257adb679b799f37b8d982066e8e116e9f3a21
yjpark <yjpark@jlkgroup.com>
2025-07-18 09:20:46 +0000

    login 수정

--- llm_env/llm_env/urls.py
+++ llm_env/llm_env/urls.py
@@ -1,16 +1,18 @@
-# llm_env/llm_env/urls.py
-
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.shortcuts import redirect
 
 urlpatterns = [
+    # 루트 경로("/")로 접속 시 바로 /evaluation/ 페이지로 이동
+    path('', lambda request: redirect('evaluation/', permanent=False)), 
+
     path('admin/', admin.site.urls),
-    
-    # 아래 라인을 주석 처리해주세요.
-    # path('accounts/', include('accounts.urls')), 
-    
+
+    # 'accounts/' 경로를 'users.urls'에 연결합니다. (수정된 부분)
+    path('accounts/', include('users.urls')), 
+
     path('inference/', include('inference.urls')),
     path('evaluation/', include('evaluation.urls')),
 ]
